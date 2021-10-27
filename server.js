2b0ba4b1ba754aeb139859b32b9bbb6187579f61
Tung <tungndhgch17584@fpt.edu.vn>
2021-10-27 22:42:14 +0700

    Initial Author Route

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+//REQUIREMENTS PACKAGE
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
+//ROUTES
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
@@ -21,5 +26,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000) 
